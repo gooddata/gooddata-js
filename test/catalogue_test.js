eed69bc0c8106ec6fc12b1eddf4d5a1b357db71e
Jan Pradac <jan.pradac@gooddata.com>
2016-06-01 15:08:35 +0200

    TRIVIAL: Moved loadCatalog tests to own describe in catalogue_test

--- test/catalogue_test.js
+++ test/catalogue_test.js
@@ -7,109 +7,103 @@ import Promise from 'bluebird';
 
 describe('Catalogue', () => {
     const projectId = 'some_id';
-    const loadCatalogUri = `/gdc/internal/projects/${projectId}/loadCatalog`;
     let ajax;
 
-    beforeEach(() => {
-        ajax = sinon.stub(xhr, 'ajax', () => Promise.resolve(fixtures.loadCatalogResponse));
-    });
+    describe('loadCatalog', () => {
+        const loadCatalogUri = `/gdc/internal/projects/${projectId}/loadCatalog`;
 
-    afterEach(() => {
-        xhr.ajax.restore();
-    });
-
-    describe('#loadItems', () => {
-        it('should load items from loadCatalog server end point', (done) => {
-            catalogue.loadItems(projectId, fixtures.optionsForEmptySelection);
-
-            setTimeout(() => {
-                const ajaxCall = ajax.getCall(0);
-                expect(ajaxCall.args[0]).to.be(loadCatalogUri);
-                expect(ajaxCall.args[1].data).to.be.eql(fixtures.requestForEmptySelection);
-                done();
-            }, 0);
+        beforeEach(() => {
+            ajax = sinon.stub(xhr, 'ajax', () => Promise.resolve(fixtures.loadCatalogResponse));
         });
 
-        it('should send maql for fact base measures', (done) => {
-            const options = fixtures.optionsForMeasureWithFilterAndCategory;
+        afterEach(() => {
+            xhr.ajax.restore();
+        });
 
-            catalogue.loadItems(projectId, options);
+        describe('#loadItems', () => {
+            it('should load items from loadCatalog server end point', (done) => {
+                catalogue.loadItems(projectId, fixtures.optionsForEmptySelection);
 
-            setTimeout(() => {
-                const ajaxCall = ajax.getCall(0);
-                const data = get(ajaxCall.args[1], 'data');
-                expect(data).to.be.eql(fixtures.requestForMeasureWithFilterAndCategory);
-                expect(get(data, 'catalogRequest.bucketItems[0]')).to.be(
-                    get(options, 'bucketItems.buckets.categories[0].category.attribute')
-                );
-                done();
+                setTimeout(() => {
+                    const ajaxCall = ajax.getCall(0);
+                    expect(ajaxCall.args[0]).to.be(loadCatalogUri);
+                    expect(ajaxCall.args[1].data).to.be.eql(fixtures.requestForEmptySelection);
+                    done();
+                }, 0);
             });
-        });
 
-        it('should send identifier for attribute base measure', (done) => {
-            const options = fixtures.optionsForTwoMeasuresFactAndAtrribute;
+            it('should send maql for fact base measures', (done) => {
+                const options = fixtures.optionsForMeasureWithFilterAndCategory;
 
-            catalogue.loadItems(projectId, options);
+                catalogue.loadItems(projectId, options);
 
-            setTimeout(() => {
-                const ajaxCall = ajax.getCall(0);
-                expect(ajaxCall.args[1].data).to.be.eql(fixtures.requestForTwoMeasureFactAndAttribute);
-                done();
-            }, 0);
-        });
-
-        it('should send maql with select identifier when visualization contains measure fact and category', (done) => {
-            const options = fixtures.optionsForMeasureWithShowInPercent;
-
-            catalogue.loadItems(projectId, options);
-
-            setTimeout(() => {
-                const ajaxCall = ajax.getCall(0);
-                const data = get(ajaxCall.args[1], 'data');
-                expect(data).to.be.eql(fixtures.requestForMeasureWithShowInPercent);
-                expect(get(data, 'catalogRequest.bucketItems[0]')).to.be(
-                    get(options, 'bucketItems.buckets.categories[0].category.attribute')
-                );
-                done();
+                setTimeout(() => {
+                    const ajaxCall = ajax.getCall(0);
+                    const data = get(ajaxCall.args[1], 'data');
+                    expect(data).to.be.eql(fixtures.requestForMeasureWithFilterAndCategory);
+                    expect(get(data, 'catalogRequest.bucketItems[0]')).to.be(
+                        get(options, 'bucketItems.buckets.categories[0].category.attribute')
+                    );
+                    done();
+                });
             });
-        });
 
-        it('should send select for fact base measure with filter', (done) => {
-            const options = fixtures.optionsForMeasureTypeFactWithFilter;
+            it('should send identifier for attribute base measure', (done) => {
+                const options = fixtures.optionsForTwoMeasuresFactAndAtrribute;
 
-            catalogue.loadItems(projectId, options);
-            setTimeout(() => {
-                const ajaxCall = ajax.getCall(0);
+                catalogue.loadItems(projectId, options);
 
-                expect(ajaxCall.args[1].data).to.be.eql(fixtures.requestForMeasureTypeFactWithFilter);
-
-                done();
+                setTimeout(() => {
+                    const ajaxCall = ajax.getCall(0);
+                    expect(ajaxCall.args[1].data).to.be.eql(fixtures.requestForTwoMeasureFactAndAttribute);
+                    done();
+                }, 0);
             });
-        });
 
-        it('should send identifier for measure type metric', (done) => {
-            const options = fixtures.optionsForMetric;
-
-            catalogue.loadItems(projectId, options);
-
-            setTimeout(() => {
-                const ajaxCall = ajax.getCall(0);
-
-                expect(ajaxCall.args[1].data).to.be.eql(fixtures.requestForMetric);
-
-                done();
+            it('should send maql with select identifier when visualization contains measure fact and category', (done) => {
+                const options = fixtures.optionsForMeasureWithShowInPercent;
+                catalogue.loadItems(projectId, options);
+                setTimeout(() => {
+                    const ajaxCall = ajax.getCall(0);
+                    const data = get(ajaxCall.args[1], 'data');
+                    expect(data).to.be.eql(fixtures.requestForMeasureWithShowInPercent);
+                    expect(get(data, 'catalogRequest.bucketItems[0]')).to.be(
+                        get(options, 'bucketItems.buckets.categories[0].category.attribute')
+                    );
+                    done();
+                });
             });
-        });
 
-        it('should not override bucketItems prop', (done) => {
-            const dummyUri = '__dummy_uri__';
-            catalogue.loadItems(projectId, { bucketItems: [dummyUri] });
-            setTimeout(() => {
-                const ajaxCall = ajax.getCall(0);
-                const { bucketItems } = ajaxCall.args[1].data.catalogRequest;
-                expect(bucketItems).to.be.eql([dummyUri]);
-                done();
-            }, 0);
+            it('should send select for fact base measure with filter', (done) => {
+                const options = fixtures.optionsForMeasureTypeFactWithFilter;
+                catalogue.loadItems(projectId, options);
+                setTimeout(() => {
+                    const ajaxCall = ajax.getCall(0);
+                    expect(ajaxCall.args[1].data).to.be.eql(fixtures.requestForMeasureTypeFactWithFilter);
+                    done();
+                });
+            });
+
+            it('should send identifier for measure type metric', (done) => {
+                const options = fixtures.optionsForMetric;
+                catalogue.loadItems(projectId, options);
+                setTimeout(() => {
+                    const ajaxCall = ajax.getCall(0);
+                    expect(ajaxCall.args[1].data).to.be.eql(fixtures.requestForMetric);
+                    done();
+                });
+            });
+
+            it('should not override bucketItems prop', (done) => {
+                const dummyUri = '__dummy_uri__';
+                catalogue.loadItems(projectId, { bucketItems: [dummyUri] });
+                setTimeout(() => {
+                    const ajaxCall = ajax.getCall(0);
+                    const { bucketItems } = ajaxCall.args[1].data.catalogRequest;
+                    expect(bucketItems).to.be.eql([dummyUri]);
+                    done();
+                }, 0);
+            });
         });
     });
 });
