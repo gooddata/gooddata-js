ef745cf1a3f06a3361f4b3e531d32c017fe3caa4
Simon Maresz <simon@salsitasoft.com>
2016-11-10 12:02:22 +0100

    RELATED: NUI-39 start admin API

---
+++ src/admin.js
@@ -0,0 +1,15 @@
+// Copyright (C) 2007-2014, GoodData(R) Corporation. All rights reserved.
+
+import * as dataProducts from './admin/dataProducts';
+
+/**
+ * Network-UI support methods. Mostly private
+ *
+ * @module admin
+ * @class admin
+ *
+ */
+
+export const admin = {
+    dataProducts
+};
