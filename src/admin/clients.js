2809fba3275422871bb6d979d860144dd3ce5dd0
Daniel Prentis <daniel@salsitasoft.com>
2016-11-18 14:28:05 +0100

    RELATED: NUI-41, NUI-45 updated api

--- src/admin/clients.js
+++ src/admin/clients.js
@@ -1,8 +1,8 @@
 import { get } from '../xhr';
 import * as routes from './routes';
 
-const transformClient = item => {
-    const params = routes.parse(item.client.links.self, routes.CONTRACT_DATA_PRODUCT_SEGMENT_DOMAIN_CLIENTS);
+const transformClient = (item) => {
+    const params = routes.parse(item.client.links.self, routes.CONTRACT_DATA_PRODUCT_DOMAIN_SEGMENTS_DOMAIN_CLIENTS);
     return {
         ...params,
         ...item.client
@@ -11,18 +11,22 @@ const transformClient = item => {
 
 export const getClients = (contractId, dataProductId, segmentId, domainId, filter, paging) => {
     const query = filter ? { clientPrefix: filter, stats: 'user' } : { stats: 'user' };
-    const uri = paging ? paging.next : routes.interpolate(routes.CONTRACT_DATA_PRODUCT_SEGMENT_DOMAIN_CLIENTS, { contractId, dataProductId, segmentId, domainId }, query);
+    const uri = paging ?
+        paging.next :
+        routes.interpolate(
+            routes.CONTRACT_DATA_PRODUCT_DOMAIN_SEGMENTS_DOMAIN_CLIENTS,
+            { contractId, dataProductId, segmentId, domainId },
+            query
+        );
 
     return get(uri).then(result => ({ items: result.client.items.map(transformClient), paging: result.client.paging }));
 };
 
 const transformClientUser = (item) => {
-    const user = item.userInClientsProject;
-    const params = routes.parse(user.links.domain, routes.CONTRACT_DOMAIN);
+    const user = item.projectUser;
     return {
         id: user.login,
-        ...params,
-        fullName: `${user.firstName} ${user.surname}`,
+        fullName: `${user.firstName} ${user.lastName}`,
         ...user
     };
 };
@@ -35,13 +39,13 @@ export const getClientUsers = (contractId, dataProductId, domainId, segmentId, c
     const uri = paging ?
         paging.next :
         routes.interpolate(
-            routes.CONTRACT_DATA_PRODUCT_SEGMENT_DOMAIN_CLIENT_USERS,
+            routes.CONTRACT_DATA_PRODUCT_DOMAIN_SEGMENTS_DOMAIN_CLIENT_USERS,
             { contractId, dataProductId, domainId, segmentId, clientId },
             query
         );
 
     return get(uri).then(result => ({
-        ...result.clientUsers,
-        items: result.clientUsers.items.map(transformClientUser)
+        ...result.clientProjectUsers,
+        items: result.clientProjectUsers.items.map(transformClientUser)
     }));
 };
