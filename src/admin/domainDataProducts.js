fc01350bcfbc16756eb057f2cd5188e4a51e4c81
Branislav Holý <branislav@salsitasoft.com>
2016-11-24 14:28:45 +0100

    RELATED: NUI-44 Fixed nested objects in response

--- src/admin/domainDataProducts.js
+++ src/admin/domainDataProducts.js
@@ -4,17 +4,14 @@ import * as routes from './routes';
 const transformDomainDataProduct = ({ domainDataProduct }) => {
     const { domainId } = routes.parse(domainDataProduct.links.domain, routes.CONTRACT_DOMAIN);
     return {
-        domainDataProduct: {
-            domainId,
-            ...domainDataProduct
-        }
+        domainId,
+        ...domainDataProduct
     };
 };
 
 export const getDomainDataProducts = (contractId, dataProductId) =>
     get(routes.interpolate(routes.CONTRACT_DATA_PRODUCT_DOMAIN_DATA_PRODUCTS, { contractId, dataProductId }))
-    .then(({ domainDataProducts: { items } }) => ({
-        domainDataProducts: {
-            items: items.map(transformDomainDataProduct)
-        }
+    .then(({ domainDataProducts: { items }, status }) => ({
+        items: items.map(transformDomainDataProduct),
+        status
     }));
