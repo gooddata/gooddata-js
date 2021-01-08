24b6dc232a19ada07b49dba6b02a4fa56cf76f56
ivanmjartan <ivan.mjartan@gooddata.com>
2021-01-08 13:23:44 +0100

    RELATED: FET-563 Fix sonar-reported issues

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,6 +1,5 @@
-// (C) 2020 GoodData Corporation
+// (C) 2020-2021 GoodData Corporation
 import { XhrModule } from "./xhr";
-
 export class BootstrapModule {
     constructor(private xhr: XhrModule) {}
 
